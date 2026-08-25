913e1fa598ec194dddd527a6307b77eb5518ca87
千落霜子 <153145450+SenAkiShimo@users.noreply.github.com>
2026-08-25 11:55:25 +0800

    update

--- phonoweave/__init__.py
+++ phonoweave/__init__.py
@@ -1,1 +1,10 @@
 __version__ = "0.0.1"
+
+from .periodicity import normalized_periodicity as _normalized_periodicity
+from . import lateral_relevance as _lateral_relevance
+from . import nasal_relevance as _nasal_relevance
+from . import rhotic_relevance as _rhotic_relevance
+
+_lateral_relevance._periodicity = _normalized_periodicity
+_nasal_relevance._periodicity = _normalized_periodicity
+_rhotic_relevance._periodicity = _normalized_periodicity
